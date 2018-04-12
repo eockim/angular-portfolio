92215b5c17ee4d10cb99aae7c17cc329fe34be0f
hyungjoon <hyungjoon6876@gmail.com>
2018-04-12 18:38:34 +0900

    message.service function add ip

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class MessageService {
   messages: string[] = [];
+  ipAddress: any;
   
-  constructor() { }
-  
+  constructor(private _http : HttpClient) {
+    this._http.get<{ip : string}>('https://jsonip.com')
+    .subscribe( data => {
+      this.ipAddress = data.ip;
+    });
+  }
+
   add(message: string){
-    this.messages.push(message);
+
+    if(!this.ipAddress){
+      this._http.get<{ip : string}>('https://jsonip.com')
+      .subscribe( data => {
+        this.ipAddress = data.ip;
+        this.messages.push('[' + this.ipAddress + ']'+ message);
+      });
+    }else{
+      this.messages.push('[' + this.ipAddress + ']'+ message);
+    }
   }
 
   clear(){
